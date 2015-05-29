4b18fb661316492be65237eef825dd0d50b66a18
Winton Yee <wyee@github.com>
2015-05-29 08:24:32 -0400

    ;)

---
+++ Tile.java
@@ -0,0 +1,54 @@
+public class Tile {
+
+    private String type;
+    private String image;
+    private String item;
+    private Baller character;
+
+    public Tile() {
+	type = "grass";
+	image = "#";
+	character = null;
+    }
+
+    public String getType() {
+	return type;
+    }
+
+    public void setType(String data) {
+	type = data;
+    }
+
+    public String getImage() {
+	return image;
+    }
+
+    public void setImage(String data) {
+	image = data;
+    }
+
+    public String getItem() {
+	return item;
+    }
+
+    public void setItem(String data) {
+	item = data;
+    }
+
+    public Baller getCharacter() {
+	return character;
+    }
+
+    public void setCharacter(Baller data) {
+	character = data;
+    }
+
+    public boolean hasCharacter() {
+	if (character == null) {
+	    return false;
+	} else {
+	    return true;
+	}
+    }
+
+}
