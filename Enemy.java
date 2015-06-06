86fe554fb870a4f90cf61ddac70946e6b45ab374
david19kerrigan <david19kerrigan@gmail.com>
2015-06-06 18:56:27 -0400

    a

---
+++ Enemy.java
@@ -0,0 +1,90 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Enemy extends Person {
+
+    private String ID;
+    private int swag;
+    private int ballskillz;
+    private int charitimasma;
+    private int rep;
+    private String direction;
+    
+    public Enemy() {
+	ID = "enemy";
+	
+	int skills = 10;
+	Random rand = new Random();
+	
+	swag = 5;
+	ballskillz = 5;
+	charitimasma = 5;
+	
+	int a = rand.nextInt(skills);
+	swag = swag + a;
+	skills = skills - a;
+
+	int b = rand.nextInt(skills);
+	ballskillz = ballskillz + b;
+	skills = skills - b;
+
+	charitimasma = charitimasma + skills;
+	rep = 1;
+
+	direction = "right";
+	
+    }
+
+    public String getID() {
+	return ID;
+    }
+    
+    public int getSwag() {
+	return swag;
+    }
+
+    public void setSwag(int n) {
+	swag += n;
+    }
+
+    public int getBallskillz() {
+	return ballskillz;
+    }
+
+    public void setBallskillz(int n) {
+	ballskillz += n;
+    }
+
+    public int getCharitimasma() {
+	return charitimasma;
+    }
+
+    public void setCharitimasma(int n) {
+	charitimasma += n;
+    }
+
+    public int getRep() {
+	return rep;
+    }
+
+    public void setRep(int n) {
+	rep += n;
+    }
+
+    public String getDirection() {
+	return direction;
+    }
+
+    public void setDirection(String s) {
+	direction = s;
+    }
+
+    public String toString() {
+	return "swag: " + swag + " ballskillz: " + ballskillz +
+	    " charitimasma: " + charitimasma;
+    }
+
+    
+
+}
